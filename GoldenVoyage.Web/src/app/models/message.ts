7bc6749d90b4900e5a5d5e2dab1f67c465804e46
lnw <gckile@hotmail.com>
2016-08-04 09:53:23 +0800

    11

--- GoldenVoyage.Web/src/app/models/message.ts
+++ GoldenVoyage.Web/src/app/models/message.ts
@@ -1,4 +1,5 @@
 ﻿export class Message {
+    subMessages: Array<Message>;
     id: number;
 
     content: string;
@@ -8,4 +9,9 @@
     fromUserId: number;
 
     toUserId: number;
+
+    constructor(param?: { content?: string, postTime?: Date }) {
+        this.content = param && param.content || "";
+        this.postTime = param && param.postTime || new Date();
+    }
 }
